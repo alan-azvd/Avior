c885e3a294ce2c7bfff8e4d31ca229b18c2be29c
Alan Azevedo <alan.azvd@gmail.com>
2017-07-30 21:54:26 -0300

    Project Modified to Nuget

--- Avior/Properties/AssemblyInfo.cs
+++ Avior/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kiso.Pages")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Avior")]
+[assembly: AssemblyDescription("A Bridge.NET UI Library based on Material Design")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kiso.Pages")]
+[assembly: AssemblyProduct("Avior")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Alan Azevedo")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
